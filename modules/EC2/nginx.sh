649b985d73d187725f3fc0fd64b90c4f78a73b5f
PSSahana <sahana280194@gmail.com>
2022-04-10 12:52:51 +0530

    app update

--- modules/EC2/nginx.sh
+++ modules/EC2/nginx.sh
@@ -1,6 +1,6 @@
+#!bin/bash
 sudo yum update -y
-sudo amazon-linux-extras install nginx1 -y
+sudo amazon-linux-extras install nginx1
 sudo systemctl start nginx.service
 sudo systemctl status nginx.service
 echo "<h1>Welcome from $(hostname -f)</h1>" > /var/www/html/index.html
-
