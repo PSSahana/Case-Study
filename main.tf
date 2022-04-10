7ce50ea35586c4de7d9f1c25babbc387e436ddc4
PSSahana <sahana280194@gmail.com>
2022-04-10 19:13:25 +0530

    bug fix

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "ec2-app-v1" {
   key-name = "test"
   ami-id = "ami-0801a1e12f4a9ccc0"
   instance-type = "t2.micro"
-  number-of-ec2-instances-required = "3"
+  number-of-ec2-instances-required = "1"
   public-key-file-name = "${file("./modules/EC2/test.pub")}"
 
   associate-public-ip-address = "true"
@@ -42,9 +42,8 @@ module "ec2-app-v1" {
   instance-name-taq = "from-EC2"
 
   #ec2-subnets-ids = ["${module.Govtech-vpc.private-subnet-ids}"]
-  
-  user-data = "${file("./modules/EC2/nginx.sh")}"
 
+  user-data = "${file("./modules/EC2/nginx.sh")}"
 
 }
 
